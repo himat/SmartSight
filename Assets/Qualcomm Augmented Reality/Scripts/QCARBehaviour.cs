3e95c479cb81f6905fe63ea9761dbc57330c4fbe
Hima T <himat15@yahoo.com>
2015-01-17 11:43:29 -0500

    First commit

---
+++ Assets/Qualcomm Augmented Reality/Scripts/QCARBehaviour.cs
@@ -0,0 +1,25 @@
+/*==============================================================================
+Copyright (c) 2010-2014 Qualcomm Connected Experiences, Inc.
+All Rights Reserved.
+Confidential and Proprietary - Qualcomm Connected Experiences, Inc.
+==============================================================================*/
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+/// <summary>
+/// The QCARBehaviour class handles tracking and triggers native video
+/// background rendering. The class updates all Trackables in the scene.
+/// </summary>
+[RequireComponent(typeof(Camera))]
+public class QCARBehaviour : QCARAbstractBehaviour
+{
+    QCARBehaviour()
+    {
+		Debug.Log("<color=red>Fatal error:</color> This thing");
+        mAndroidUnityPlayer = new AndroidUnityPlayer();
+    }
+}
---
+++ Assets/Qualcomm Augmented Reality/Scripts/TestMovement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class TestMovement : ImageTargetBehaviour
+{
+
+		// Use this for initialization
+		void Start ()
+		{
+		Debug.Log("<color=red>Fatal error:</color> This thing");
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+}
+

f841b167949c032a6e1e7c22c6a5f3d2f68f6505
Evorsio <anuraag.pi@gmail.com>
2015-01-18 06:31:25 -0500

    some changes of login

--- Assets/LoginMenu.cs
+++ Assets/LoginMenu.cs
@@ -8,6 +8,7 @@ using SimpleJSON;
 
 public class LoginMenu : MonoBehaviour {
 	//actually about menu
+	private GUIStyle Title = new GUIStyle();
 	private GUIStyle Texty = new GUIStyle();
 	public Font MyFont;
 
@@ -31,15 +32,19 @@ public class LoginMenu : MonoBehaviour {
 		GUI.skin.font = MyFont;
 		Texty.fontSize = 65;
 		Texty.normal.textColor = Color.white;
-		GUI.Label (new Rect (Screen.width / 80, Screen.height / 80 , 100, 200), "Capital One account first and last name:", Texty);
+		Title.fontSize = 200;
+		Title.normal.textColor = Color.white;
 
-		name = GUI.TextField (new Rect (Screen.width / 80 + 100, Screen.height / 50, 400, 200), "First and last name");
+		GUI.Label (new Rect (Screen.width / 3, Screen.height / 80 , 100, 200), "Capital One Account", Title);
+		GUI.Label (new Rect (Screen.width / 3, Screen.height / 60 , 100, 200), "Please Enter Your First and Last Name:", Texty);
+
+		name = GUI.TextField (new Rect (Screen.width / 80, Screen.height / 50, 400, 200), "First and last name", Texty);
 
 
-		if (GUI.Button(new Rect (Screen.width / 80 +500, Screen.height/350, 300, 200), "Confirm")) {
+		if (GUI.Button(new Rect (0 , Screen.height/30, 300, 200), "1")) {
 			Application.LoadLevel ("menu");
 		}
-		if (GUI.Button(new Rect (Screen.width / 2 - Screen.width/12,Screen.height/2 + Screen.height/4, 300, 200), "")) {
+		if (GUI.Button(new Rect (Screen.width/2 - Screen.width /3,Screen.height/30, 300, 200), "2")) {
 			Application.LoadLevel ("menu");
 		}
 

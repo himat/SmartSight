4f14ba3b4110362bc60d6615c3d048c17a7577a2
Evorsio <anuraag.pi@gmail.com>
2015-01-18 06:52:11 -0500

    re areange login

--- Assets/LoginMenu.cs
+++ Assets/LoginMenu.cs
@@ -32,23 +32,23 @@ public class LoginMenu : MonoBehaviour {
 		GUI.skin.font = MyFont;
 		Texty.fontSize = 65;
 		Texty.normal.textColor = Color.white;
-		Title.fontSize = 200;
+		Title.fontSize = 170;
 		Title.normal.textColor = Color.white;
 
-		GUI.Label (new Rect (Screen.width / 3, Screen.height / 80 , 100, 200), "Capital One Account", Title);
-		GUI.Label (new Rect (Screen.width / 3, Screen.height / 60 , 100, 200), "Please Enter Your First and Last Name:", Texty);
+		GUI.Label (new Rect (Screen.width / 80, Screen.height / 80 , 100, 200), "Capital One Login", Title);
+		GUI.Label (new Rect (Screen.width / 80, Screen.height / 5 + Screen.height / 20, 100, 200), "Please Enter Your  \n First and Last Name:", Texty);
 
-		name = GUI.TextField (new Rect (Screen.width / 80, Screen.height / 50, 400, 200), "First and last name", Texty);
+		name = GUI.TextField (new Rect (Screen.width / 80, Screen.height / 4 + Screen.height / 9, 600, 150), "");
 
 
-		if (GUI.Button(new Rect (0 , Screen.height/30, 300, 200), "1")) {
+		if (GUI.Button(new Rect (Screen.width/100 , Screen.height/3 *2, 300, 200), "")) {
 			Application.LoadLevel ("menu");
 		}
-		if (GUI.Button(new Rect (Screen.width/2 - Screen.width /3,Screen.height/30, 300, 200), "2")) {
+		if (GUI.Button(new Rect (Screen.width/2 - Screen.width /3,Screen.height/3 *2, 300, 200), "")) {
 			Application.LoadLevel ("menu");
 		}
 
-		GUI.Label (new Rect (Screen.width / 2 - Screen.width/50 - Screen.width/30, Screen.height / 2 + Screen.height/4 + Screen.height/15, 200, 100), "Back", Texty);
+		GUI.Label (new Rect (Screen.width/2 - Screen.width /3, Screen.height/3 *2, 200, 100), "Back", Texty);
 	}
 }
 

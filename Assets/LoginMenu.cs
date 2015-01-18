d50ee60979417f4339f542914dcda728d57816ee
Evorsio <anuraag.pi@gmail.com>
2015-01-18 07:14:01 -0500

    re areange login

--- Assets/LoginMenu.cs
+++ Assets/LoginMenu.cs
@@ -46,20 +46,10 @@ public class LoginMenu : MonoBehaviour {
 		Title.normal.textColor = Color.white;
 
 		GUI.Label (new Rect (Screen.width / 80, Screen.height / 80 , 100, 200), "Capital One Login", Title);
-		GUI.Label (new Rect (Screen.width / 80, Screen.height / 5 + Screen.height / 20, 100, 200), "Please Enter Your  \n First and Last Name:", Texty);
+		GUI.Label (new Rect (Screen.width / 80, Screen.height / 5 + Screen.height / 20, 100, 200), "Please Enter Your  \nFirst and Last Name:", Texty);
+		enteredName = GUI.TextField (new Rect (Screen.width / 80, Screen.height / 4 + Screen.height / 8, 600, 150), "");
 
-		name = GUI.TextField (new Rect (Screen.width / 80, Screen.height / 4 + Screen.height / 9, 600, 150), "");
-
-
-		if (GUI.Button(new Rect (Screen.width/100 , Screen.height/3 *2, 300, 200), "")) {
-			Application.LoadLevel ("menu");
-
-		name = GUI.TextField (new Rect (Screen.width / 80, Screen.height / 50, 400, 200), "First and last name", Texty);
-
-
-		if (GUI.Button(new Rect (0 , Screen.height/30, 300, 200), "1")) {
-
-				enteredName = GUI.TextField (new Rect (Screen.width / 80 + 100, Screen.height / 50, 400, 200), "First and last name");
+		if (GUI.Button(new Rect (0 , Screen.height/3 *2, 300, 200), "1")) {
 			
 			if(enteredName == accountName)
 				isLoggedIn = true;
@@ -70,13 +60,13 @@ public class LoginMenu : MonoBehaviour {
 				GUI.Label (new Rect (Screen.width / 2 - Screen.width/50 - Screen.width/30, Screen.height / 2 + Screen.height/4 + Screen.height/15, 200, 100), accountName + "::" + enteredName, Texty);
 
 		}
-		if (GUI.Button(new Rect (Screen.width/2 - Screen.width /3,Screen.height/3 *2, 300, 200), "")) {
+		if (GUI.Button(new Rect (Screen.width/2 - Screen.width /3,Screen.height/3 *2, 300, 200), "2")) {
 			Application.LoadLevel ("menu");
 		}
 
-		GUI.Label (new Rect (Screen.width/2 - Screen.width /3, Screen.height/3 *2, 200, 100), "Back", Texty);
+		GUI.Label (new Rect (Screen.width/2 - Screen.width /3 + Screen.width/50, Screen.height/3 *2 + Screen.height/15, 200, 100), "Back", Texty);
+		GUI.Label (new Rect (Screen.width/90, Screen.height/3 *2 + Screen.height/15, 200, 100), "Confirm", Texty);
 	}
-}
 }
 
 /*using UnityEngine;
